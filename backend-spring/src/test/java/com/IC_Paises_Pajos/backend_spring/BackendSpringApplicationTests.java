98b5a97708f21fef48f43b9aafef4bcff42d6590
Misaelvilla <villagomezmisael@gmail.com>
2025-04-08 11:57:37 +0200

    Añadido soporte para archivo de configuración externo .env.properties

---
+++ backend-spring/src/test/java/com/IC_Paises_Pajos/backend_spring/BackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IC_Paises_Pajos.backend_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
